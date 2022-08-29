820d9a251bf42ed143bebed0fd0a94587c0de43d
Adam Szalkowski <adam@szalkowski.de>
2022-08-29 07:57:59 +0200

    feat: repeating keys

--- src/controls.rs
+++ src/controls.rs
@@ -19,7 +19,12 @@ pub enum ControlEvent {
     RotateLeft,
 }
 
-fn controls(keys: Res<Input<KeyCode>>, mut events: EventWriter<ControlEvent>) {
+fn controls(
+    keys: Res<Input<KeyCode>>,
+    mut events: EventWriter<ControlEvent>,
+    time: Res<Time>,
+    mut repeat_timer: Local<Timer>,
+) {
     if keys.just_pressed(KeyCode::Space) {
         events.send(ControlEvent::Pause);
     }
@@ -32,20 +37,26 @@ fn controls(keys: Res<Input<KeyCode>>, mut events: EventWriter<ControlEvent>) {
         events.send(ControlEvent::SpeedupEnd);
     }
 
-    if keys.just_pressed(KeyCode::Right) {
-        events.send(ControlEvent::Right);
-    }
-
-    if keys.just_pressed(KeyCode::Left) {
-        events.send(ControlEvent::Left);
-    }
-
-    if keys.just_pressed(KeyCode::Up) {
-        events.send(ControlEvent::RotateRight);
-    }
-
     let shift = keys.any_pressed([KeyCode::LShift, KeyCode::RShift]);
-    if shift && keys.pressed(KeyCode::Up) {
-        events.send(ControlEvent::RotateLeft);
-    }
+    let mut handle_repeating_key = |key_code: KeyCode, control_event: ControlEvent, delay: f32| {
+        if keys.just_pressed(key_code) {
+            events.send(control_event);
+            *repeat_timer = Timer::from_seconds(delay, false);
+            true
+        } else if keys.pressed(key_code) {
+            repeat_timer.tick(time.delta());
+            if repeat_timer.just_finished() {
+                events.send(control_event);
+                *repeat_timer = Timer::from_seconds(0.1, false);
+            }
+            true
+        } else {
+            false
+        }
+    };
+
+    let _ = handle_repeating_key(KeyCode::Right, ControlEvent::Right, 0.3)
+        || handle_repeating_key(KeyCode::Left, ControlEvent::Left, 0.3)
+        || handle_repeating_key(KeyCode::Up, ControlEvent::RotateRight, 0.6)
+        || (shift && handle_repeating_key(KeyCode::Up, ControlEvent::RotateLeft, 0.6));
 }
